29a570b7c2fcd48c20ce44c9c22808a75ee2ab49
SATHANASELLAMUTHU <49227033+SATHANASELLAMUTHU@users.noreply.github.com>
2019-04-10 11:48:10 +0530

    B4.py

---
+++ B4.py
@@ -0,0 +1,5 @@
+n=input("Enter the character:")
+if(n=="a"or n=="b"orn=="c"or n=="d"orn=="e"or n=="f"orn=="g"or n=="h"orn=="i"or n=="j"orn=="k"or n=="l"orn=="m"or n=="n"orn=="o"or n=="p"orn=="q"or n=="r"or n=="s"orn=="t"or n=="u"or n=="v"or n=="w"or n=="x"or n=="y"or n=="z"):
+  print("Alphabet")
+else:
+  print("Not a alphabet)
