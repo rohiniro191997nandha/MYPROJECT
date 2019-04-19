95bdb98697f90c590047554a8e24850f51b50230
SATHANASELLAMUTHU <49227033+SATHANASELLAMUTHU@users.noreply.github.com>
2019-04-19 23:45:37 +0530

    B52.py

---
+++ B52.py
@@ -0,0 +1,22 @@
+x=int(input("Enter the number:"))
+if(x==1):
+  print("One")
+if(x==2):
+  print("Two")
+if(x==3):
+  print("Three")
+if(x==4):
+  print("Four")
+if(x==5):
+  print("Five")
+if(x==6):
+  print("Six")
+if(x==7):
+  print("Seven")
+if(x==8):
+  print("Eight")
+if(x==9):
+  print("Nine")
+if(x==10):
+  print("Ten")
+ 
