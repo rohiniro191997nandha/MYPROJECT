bab963a836c09192e7cacba9064663fc25745c6f
SATHANASELLAMUTHU <49227033+SATHANASELLAMUTHU@users.noreply.github.com>
2019-04-23 16:08:54 +0530

    B21.py

--- B21.py
+++ B21.py
@@ -1,12 +1,12 @@
 def sum1(b,c,a):
 sum=0
 i=0
-while<a:
+while i<a:
 sum=sum+b
 b=b+c
 i=i+1
 return sum
-a=int(input("n1")
-b=int(input("n2")
-c=int(input("n3")
+a=int(input("n1"))
+b=int(input("n2"))
+c=int(input("n3"))
 print(sum1(b,c,a))
