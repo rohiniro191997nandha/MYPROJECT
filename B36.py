c560ed1811b21b522b962ee683a21b363141dd2d
SATHANASELLAMUTHU <49227033+SATHANASELLAMUTHU@users.noreply.github.com>
2019-05-05 16:50:39 +0530

    B36.py

--- B36.py
+++ B36.py
@@ -1,6 +1,6 @@
 n=raw_input("Enter the number or character or special character:")
 c=0
 for i in n:
-  if(i=='~' or i=='`' or i=='!' or i=='@' or i=='#' or i=='$' or i=='%' or i=='^' or i=='&' or i=='*' or i=='(' or i==')' or i=='-' or i=='_' or i=='=' or i=='+' or i=='[' or i==']' or i=='{' or i=='}' or i=='\' or i=='|' or i==';' or i==':' or i=='"' or i==',' or i=='<' or i=='.' or i=='>' or i=='/' or i=='?'): 
+  if(i=='~' or i=='`' or i=='!' or i=='@' or i=='#' or i=='$' or i=='%' or i=='^' or i=='&' or i=='*' or i=='(' or i==')' or i=='-' or i=='_' or i=='=' or i=='+' or i=='[' or i==']' or i=='{' or i=='}' or i=='|' or i==';' or i==':' or i=='"' or i==',' or i=='<' or i=='.' or i=='>' or i=='/' or i=='?'): 
   c=c+1
   print(c)
